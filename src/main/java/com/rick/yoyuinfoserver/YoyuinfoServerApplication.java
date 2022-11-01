c41deaa768a40dbd398166c81fa68996f2712fd9
Yasuomvp <teemovv@outlook.com>
2022-11-01 17:48:06 +0800

    Initial commit

---
+++ src/main/java/com/rick/yoyuinfoserver/YoyuinfoServerApplication.java
@@ -0,0 +1,13 @@
+package com.rick.yoyuinfoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoyuinfoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoyuinfoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rick/yoyuinfoserver/YoyuinfoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick.yoyuinfoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoyuinfoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
